7919941fb0e8d9931a75e93f8aee7d2727617f46
Josh <jish.bowen@gmail.com>
2015-09-07 17:41:22 -0600

    Update before purging laptop

---
+++ Qt/build-sqrt-Desktop-Default/CMakeFiles/sqrt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sqrt.dir/main.cpp.o"
+  "sqrt.pdb"
+  "sqrt"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sqrt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
